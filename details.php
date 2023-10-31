f96b3a705e2d6b4974e400da28eb52acc79da30d
Webase Solutions <webase.master@gmail.com>
2023-10-31 19:00:50 +0600

    first commit

---
+++ details.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Details</title><link href="https://fonts.googleapis.com/css2?family=Baloo+Da+2:wght@400;500;600;700&family=Noto+Sans+Bengali:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <section class="m-view">
+    <div class="hero-section">
+      <div class="my-container">
+        <div class="section-content">
+          <div class="row d-flex align-items-center justify-content-center">
+            <div class="col-6 flex-end">
+              <div class="hero-content text-center">
+                <img src="img/man.png" alt="">
+                <h2>আসাদুজ্জামান খান কামাল<br> কে ঢাকা ১২ আসনে</h2>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="hero-img text-start">
+                <img src="img/boat.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-content">
+      <div class="my-container">
+        <div class="details-table">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th style="width: 5%;">ক্রমিক</th>
+                <th style="width: 10%;">নাম</th>
+                <th style="width: 10%;">পিতা/স্বামী</th>
+                <th style="width: 10%;">জন্ম তারিখ</th>
+                <th style="width: 15%;">ঠিকানা</th>
+                <th style="width: 15%;">ভোট কেন্দ্র</th>
+                <th style="width: 20%;">কক্ষ</th>
+                <th style="width: 15%;">SMS</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>০০৯৭</td>
+                <td>মোছাঃ সালমা খাতুন</td>
+                <td>মোঃ বেলাল</td>
+                <td>১০/১০/১৯৭৮</td>
+                <td>মধুবাগ, ৩৫৭/১৪/৩ (১৭), রমনা, ঢাকা</td>
+                <td>শের-ই বাংলা স্কুল এন্ড কলেজ, মধুবাগ, মগবাজার</td>
+                <td>(পশ্চিম পাশের কলেজ ভবন,নীচ তলা ও ২য় তলা),(মহিলা ভোটার -১), কেন্দ্র-৩</td>
+                <td><a href="" data-bs-target="#exampleModalToggle" data-bs-toggle="modal" class="send-sms">মোবাইল এ SMS পাঠান</a></td>
+              </tr>
+              <tr>
+                <td>০০৯৭</td>
+                <td>মোছাঃ সালমা খাতুন</td>
+                <td>মোঃ বেলাল</td>
+                <td>১০/১০/১৯৭৮</td>
+                <td>মধুবাগ, ৩৫৭/১৪/৩ (১৭), রমনা, ঢাকা</td>
+                <td>শের-ই বাংলা স্কুল এন্ড কলেজ, মধুবাগ, মগবাজার</td>
+                <td>(পশ্চিম পাশের কলেজ ভবন,নীচ তলা ও ২য় তলা),(মহিলা ভোটার -১), কেন্দ্র-৩</td>
+                <td><a href="" data-bs-target="#exampleModalToggle" data-bs-toggle="modal" class="send-sms">মোবাইল এ SMS পাঠান</a></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+          <div class="text-center">
+            <a href="" class="search">পুনরায় অনুসন্ধান করুন</a>
+          </div>
+      </div>
+      <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered modal-xl">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalToggleLabel">ভোট কেন্দ্রের তথ্য পেতে আপনার ফোন নম্বর লিখুন</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="phone-save">
+                <input type="text" class="" placeholder="মোবাইল নম্বর লিখুন">
+                <a href="" class="phone-send">মোবাইল নম্বর সেন্ড করুন</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vote</title><link href="https://fonts.googleapis.com/css2?family=Baloo+Da+2:wght@400;500;600;700&family=Noto+Sans+Bengali:wght@400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  
+</head>
+<body>
+  <section class="m-view">
+    <div class="hero-section">
+      <div class="my-container">
+        <div class="section-content">
+          <div class="row d-flex align-items-center justify-content-center">
+            <div class="col-6 flex-end flex-sm-center">
+              <div class="hero-content text-center">
+                <img src="img/man.png" alt="">
+                <h2>আসাদুজ্জামান খান কামাল<br> কে ঢাকা ১২ আসনে</h2>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="hero-img text-start">
+                <img src="img/boat.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-content">
+      <div class="my-container">
+        <div class="row">
+          <div class="col-12">
+            <div class="contact-area">
+              <h3>আপনার ভোট কেন্দ্রের নাম জানতে আপনার তথ্য বাংলায় প্রদান করুন</h3>
+              <form action="">
+                <div class="row">
+                  <div class="col-md-3 mb-3">
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="নাম">
+                  </div>
+                  <div class="col-md-3 mb-3">
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="পিতার নাম">
+                  </div>
+                  <div class="col-md-3 mb-3">
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="জন্ম তারিখ">
+                  </div>
+                  <div class="col-md-3 mb-3">
+                    <div class="mb-3">
+                      <select class="form-select" aria-label="Default select example">
+                        <option selected>ওয়ার্ড</option>
+                        <option value="1">এক</option>
+                        <option value="2">দুই</option>
+                        <option value="3">তিন</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div class="text-center">
+                <input type="submit" value="অনুসন্ধান" class="search">
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
